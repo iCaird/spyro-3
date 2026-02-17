3fdf939bac0d5b990ed1dc39de5fa339b016f8ce
Ellis Caird <ellisc15060@gmail.com>
2026-02-17 02:28:13 +0000

    added func_level_43_8008C5AC

--- src/overlays/level_43/level.c
+++ src/overlays/level_43/level.c
@@ -1,4 +1,9 @@
 #include "common.h"
+#include "mobyutil.h"
+
+#include "stdutil.h"
+
+#include "ovl_header.h"
 
 INCLUDE_ASM("asm/nonmatchings/overlays/level_43/level", func_level_43_8008B598);
 
@@ -12,7 +17,27 @@ INCLUDE_ASM("asm/nonmatchings/overlays/level_43/level", func_level_43_8008BF10);
 
 INCLUDE_ASM("asm/nonmatchings/overlays/level_43/level", func_level_43_8008C498);
 
-INCLUDE_ASM("asm/nonmatchings/overlays/level_43/level", func_level_43_8008C5AC);
+
+extern Spyro spyro;
+extern void func_8004F178(Vector3D*, Vector3D*); // fSetVector
+int func_level_43_8008C5AC(Vector3D* arg0, int arg1, Moby* arg2) {
+    int temp_s0;
+    int temp_s1;
+    
+    temp_s1 = arg1;
+    temp_s0 = spyro.unk17c;
+    
+    spyro.unk17a = 0x10000040;
+    spyro.unk17d = arg2;
+
+    temp_s0 = temp_s0 == 2;
+    
+    func_8004F178(&spyro.unk19, arg0);
+    
+    spyro.unk20[0] = temp_s1;
+    
+    return temp_s0;
+}
 
 INCLUDE_ASM("asm/nonmatchings/overlays/level_43/level", func_level_43_8008C620);
 
