fd1f49a380d775f70167b94382042ad53ab7a52f
Ellis Caird <ellisc15060@gmail.com>
2026-02-21 10:45:06 +0000

    PR requests

--- src/overlays/level_42/level.c
+++ src/overlays/level_42/level.c
@@ -1,25 +1,25 @@
+#include "camera.h"
 #include "common.h"
 #include "spyro.h"
-INCLUDE_ASM("asm/nonmatchings/overlays/level_42/level", func_level_42_80084AB8);
 
-extern int D_8006E044;
-extern unsigned int D_8006E048;
-extern Spyro spyro;
+extern Camera g_Camera;
+
+INCLUDE_ASM("asm/nonmatchings/overlays/level_42/level", func_level_42_80084AB8);
 
 /*
  * ???() - func_level_42_80084DD4 - MATCHED
  * https://decomp.me/scratch/atGPB
  */
-void func_level_42_80084DD4(Moby* arg0, int arg1) {
+void func_level_42_80084DD4(Moby* moby, int arg1) {
     spyro.unk20a = 1;
     spyro.unk20b = arg1;
-    spyro.critterMobyPtr = arg0;
-    if (((D_8006E044 == 7) && ((unsigned int) D_8006E048 >= 2U)) || (spyro.unk17b & 0x2000)) {
-        arg0->lowDrawDistance = 0;
-        arg0->drawn = 0;
+    spyro.critterMobyPtr = moby;
+    if (((g_Camera.cameraState == CAMERA_FIRST_PERSON) && ((unsigned int) g_Camera.unk50 >= 2U)) || (spyro.unk17b & 0x2000)) {
+        moby->lowDrawDistance = 0;
+        moby->drawn = 0;
         return;
     }
-    arg0->lowDrawDistance = 0x10;
+    moby->lowDrawDistance = 0x10;
 }
 
 
@@ -30,13 +30,13 @@ INCLUDE_ASM("asm/nonmatchings/overlays/level_42/level", func_level_42_80084E4C);
  * ???() - func_level_42_80084DD4 - MATCHED
  * https://decomp.me/scratch/oWPff
  */
-int func_level_42_800852D0(Vector3D* arg0, int arg1, Moby* arg2) {
+int func_level_42_800852D0(Vector3D* vec, int arg1, Moby* moby) {
     int temp_s0;
 
     temp_s0 = spyro.unk17c == 2;
     spyro.unk17a = 0x10000040;
-    spyro.unk17d = arg2;
-    func_8004F178(&spyro.unk19, arg0);
+    spyro.unk17d = moby; //whirlwind
+    func_8004F178(&spyro.unk19, vec); //fSetVector
     spyro.unk20[0] = arg1;
     return temp_s0;
 }
